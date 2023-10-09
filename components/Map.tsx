13251232d734da7762cd180e1e5db910f1e3c1c9
tchi-sama <tchisama41@gmail.com>
2023-10-09 04:08:56 -0700

    going good

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet,Image,Text, View } from 'react-native'
+import { StyleSheet,Image,Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
-import MapView , {Marker} from 'react-native-maps'
+import MapView , {Marker,Callout} from 'react-native-maps'
 import { theme } from '../constants/Colors'
 
 type Props = {}
@@ -230,7 +230,16 @@ const Map = (props: Props) => {
         longitude: -6.391043775400629,
       };
   return (
-    <MapView  customMapStyle={mapStyling} style={styles.map} >
+    <MapView  
+        customMapStyle={mapStyling} 
+        style={styles.map} 
+        initialRegion={{
+            latitude: 32.359774892258095,
+            longitude: -6.391043775400629,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+        }} 
+        >
         {/* add me a custom marker */}
         <Marker coordinate={markerCoordinates}>
             <View style={styles.imageContainer}>
@@ -239,12 +248,28 @@ const Map = (props: Props) => {
                 style={styles.meCustomMarker}
                 />
             </View>
+            <Callout tooltip={false} style={styles.callout}>
+            <View >
+              <View style={{flexDirection:'row',justifyContent:"space-between",alignItems:"flex-start"}}>
+                <Text style={styles.name}>tchisama</Text>
+                {
+                    true ?
+                    <Text style={styles.badgFree}>Free</Text>
+                    :
+                    <Text style={styles.badgBusy}>Busy</Text>
+                }
+              </View>
+                <Text style={{fontSize:12}}>full stack web developer 💻</Text>
+                <Text style={{fontSize:12}}>22 years / male</Text>
+                <Text style={{fontSize:10, color:'gray'}}>contact me and lets work together Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, corporis, quidem provident voluptate suscipit hic perferendis dolor veniam itaque natus a eaque quasi dolore modi vitae non! Quo, maiores officiis!</Text>
+            </View>
+          </Callout>
         </Marker>
-        <Marker coordinate={{latitude: 30.359774892258095, longitude: -6.492043775500639}}>
+        <Marker  coordinate={{latitude: 30.359774892258095, longitude: -6.492043775500639}}>
             <View style={styles.imageContainer}>
                 <Image
-                source={{uri:'https://avatars.githubusercontent.com/u/115560200?v=4'}}
-                style={styles.customMarker}
+                   source={{uri:'https://avatars.githubusercontent.com/u/25749407?v=4'}}
+                   style={styles.customMarker}
                 />
             </View>
         </Marker>
@@ -256,6 +281,43 @@ const Map = (props: Props) => {
 export default Map
 
 const styles = StyleSheet.create({
+    name:{
+        fontSize:18,
+
+    },
+    bage:{
+        padding:2,
+        paddingHorizontal:8,
+        borderRadius:30,
+        fontSize:10,
+        color:"white",
+    },
+    badgFree:{
+        padding:2,
+        paddingHorizontal:8,
+        borderRadius:30,
+        fontSize:10,
+        color:"white",
+        backgroundColor:theme.light.primery,
+    },
+    badgBusy:{
+        fontSize:10,
+        color:"white",
+        backgroundColor:"tomato",
+        padding:2,
+        paddingHorizontal:8,
+        borderRadius:30,
+    },
+    button:{
+        backgroundColor:theme.light.primery,
+        padding: 6,
+        paddingHorizontal:12,
+        color:"white",
+        borderRadius:30,
+        marginTop:10,
+        justifyContent:"center",
+        alignItems:"center",
+    },
   map:{
     flex: 1,
     backgroundColor: "#f3f4f6",    
@@ -271,17 +333,22 @@ const styles = StyleSheet.create({
     backgroundColor:'white',
   },
   meCustomMarker:{
-    borderWidth:2,
-    borderColor:theme.light.primery+"88",
+    borderWidth:1,
+    borderColor:"#fff",
     width: 32,
     height: 32,
     borderRadius:40,
   },
   customMarker:{
+
     borderWidth:2,
-    borderColor:'white',
     width: 30,
     height: 30,
     borderRadius:40,
+  },
+  callout:{
+    width: 250,
+    height: "auto",
+    padding: 6,
   }
 })
