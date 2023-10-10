b191c24722673dfe4bf2fb22bc1263d8ab9cd0ce
tchi-sama <tchisama41@gmail.com>
2023-10-10 08:26:57 -0700

    yep 3

--- hooks/categories.ts
+++ hooks/categories.ts
@@ -3,7 +3,6 @@ import {create} from 'zustand';
 const useCatStore = create<{ category: number, setCategory: (cat: number) => void}>((set) => ({
   category: 0,
   setCategory: (cat: number) => set({ category:cat })
-  
 }));
 
 export default useCatStore;
